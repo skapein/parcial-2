3235c7547ded0392056a920508e9609f06df70bf
chrisMJ <chris666.10@hotmail.com>
2024-03-22 12:16:00 -0500

    correccion modulo usaurios

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { USER } from 'src/models/models';
+import { UserSchema } from './schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: USER.name,
+        useFactory: () => {
+          return UserSchema;
+        },
+      },
+    ]),
+  ],
+  controllers: [UsersController],
   providers: [UsersService],
-  controllers: [UsersController]
+  exports: [UsersService],
 })
 export class UsersModule {}
